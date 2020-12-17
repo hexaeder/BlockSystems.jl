ad82143580dd555b9dde3bf67cdd09bddd917218
Documenter.jl <documenter@juliadocs.github.io>
2020-12-17 14:55:59 +0000

    build based on 7ed6ed5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = IOSystems","category":"page"},{"location":"#IOSystems","page":"Home","title":"IOSystems","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [IOSystems]","category":"page"}]
+}
