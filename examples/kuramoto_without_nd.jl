7eaabaf60a782a157253bd9ea71bfe057e7cd980
Hans Würfel <git@wuerfel.io>
2021-03-29 08:51:18 +0200

    update network example to nd0.5 style (#22)
    * update example to nd0.5 style
    
    Co-authored-by: lindnemi <michaellindner@pik-potsdam.de>

--- examples/kuramoto_without_nd.jl
+++ examples/kuramoto_without_nd.jl
@@ -1,9 +1,11 @@
 #=
 ## Network Dynamics without NetworkDynamics.jl
-In this example we want to explore the same problem as in [Integration with `NetworkDynamics.jl`](@ref).
-But this time without `NetworkDynamics.jl` ...
 =#
-
+#src In this example we want to explore the same problem as in [Integration with `NetworkDynamics.jl`](@ref).
+#src But this time without `NetworkDynamics.jl` ...
+#=
+In this example we model a Kuramoto system on a complex network.
+=#
 using LightGraphs
 using BlockSystems
 using ModelingToolkit
@@ -11,78 +13,98 @@ using OrdinaryDiffEq
 using Plots
 
 #=
-The goal is to generate IOBlocks for edges and vertices based on a given graph. In order to do so we have to make the vertex blocks a bit special: since MDK does not support vector inputs we need a special IOBlock for each Vertex depending on the outgoing and incoming edges as inputs.
+Dynamical systems on complex networks are best modelled on directed graphs, because in
+general an interaction of node i and j may be asymmetric, e.g. when node i influences
+node j but not vice versa.
+
+Symmetries of the system such as (anti-)symmetric coupling functions may justify other
+approaches, but those are of limited scope, for a detailed discussion see
+[our paper](https://arxiv.org/abs/2012.12696), Section II.D.
+
+In the following each edge is represented by a function:
+
+```
+     e₁₂ = f(1,2)
+     .--->---.
+   (1)       (2)
+     ˙---<---˙
+     e₂₁ = f(2,1)
+```
+
+Each edge function sees the values of the connected nodes, its source and its destination.
+Nodes are modeled as functions as well and see all their *incoming* edges.
+
+The goal is to generate IOBlocks for edges and vertices based on a given graph.
+
+While defining the edge block is straightforward, we have to make the vertex blocks a bit
+special: since MTK does not support vector inputs yet we need a special IOBlock which
+depends on the number of connected edges.
 =#
 function gen_edge_block(name)
     @parameters t src(t) dst(t) K
-    @variables o(t)
-    IOBlock([ o ~ K*sin(src-dst) ], [src,dst], [o], name=Symbol(name))
+    @variables out(t)
+    IOBlock([out ~ K*sin(src-dst)], [src, dst], [out], name=Symbol(name))
 end
 
-function gen_vertex_block(n_in, n_out, name)
-    @parameters t ω edgesum(t)
-    @parameters in_edge[1:n_in](t)
-    @parameters out_edge[1:n_out](t)
+function gen_vertex_block(n_edges, name)
+    @parameters t ω
+    @parameters edge[1:n_edges](t)
     @variables ϕ(t)
     D = Differential(t)
 
-    rhs = ω
-    if n_in > 0
-        rhs += (+)(in_edge...)
-    end
-    if n_out > 0
-        rhs -= (+)(out_edge...)
-    end
-
-    IOBlock([D(ϕ) ~ rhs],
-            [in_edge..., out_edge...],
+    IOBlock([D(ϕ) ~ ω + (+)(edge...)],
+            [edge...],
             [ϕ],
             name=Symbol(name))
 end
 nothing #hide
 
 #=
-The graph is defined as in the other example.
+For simplicity our graph will be a simple ring network specified with LightGraphs.jl
+
 =#
 N = 8
-g = watts_strogatz(N,2,0) # ring network
+g = SimpleDiGraph(watts_strogatz(N,2,0)) # ring network
 nothing #hide
 
-# First we generate a list of all edge-blocks because the don't depend on the vertices.
-edgelist = [(i=i, src=e.src, dst=e.dst, block=gen_edge_block("$(e.src)to$(e.dst)")) for (i, e) in enumerate(edges(g))]
+# First we generate a list of all edge-blocks because they don't depend on the vertices.
+
+edgelist = [(i=i, src=e.src, dst=e.dst, block=gen_edge_block("e_$(e.src)_$(e.dst)"))
+            for (i, e) in enumerate(edges(g))]
 edge_blocks = [e.block for e in edgelist]
 nothing #hide
 
 #=
-With the edges we can generate vert blocks based on their number of out and in edges.
-We can also create the connections
+Now we can generate vertex blocks based on their number of incoming edges.
+We will also create the connections
 ```
-e1to2.src => v1.out_edge₁
-e1to2.dst => v2.in_edge₁
+e_i_k.out => node.edge₁
+e_j_k.out => node.edge₂
+...
 ```
-and so forth.
+for all edges that go from vertices i or j to vertex k.
 =#
 vert_blocks = IOBlock[]
 connections = Pair[]
 
 for i in vertices(g)
-    out_edges = filter(e->e.src == i, edgelist)
-    in_edges = filter(e->e.dst == i, edgelist)
-    block = gen_vertex_block(length(in_edges), length(out_edges), "v$i")
-    push!(vert_blocks, block)
+    ## collect the incoming edges for each node
+    edges = filter(e -> e.dst == i, edgelist)
 
-    for (i, edge) in enumerate(out_edges)
-        input = getproperty(block, Symbol("out_edge", Char(0x02080 + i)))
-        push!(connections, edge.block.o => input)
-    end
+    node = gen_vertex_block(length(edges), "v$i")
+    push!(vert_blocks, node)
 
-    for (i, edge) in enumerate(in_edges)
-        input = getproperty(block, Symbol("in_edge", Char(0x02080 + i)))
-        push!(connections, edge.block.o => input)
+    ## each node has the open inputs edge₁, edge₂, ...
+    ## we need to connect the outputs of the edge-blocks to the
+    ## inputs of the node like edge_j_to_1.out => node.edge₁
+    for (i, edge) in enumerate(edges)
+        node_input_i = getproperty(node, Symbol("edge", Char(0x02080 + i)))
+        push!(connections, edge.block.out => node_input_i)
     end
 end
 
-# Once the vertices are generated we can plug the edges src and dst to the output of the corresponding vertex.
+# Once the vertices are generated we can plug the edges' src and dst to the output of the #
+# corresponding vertex, in this case its the oscillators angle ϕ
 for edge in edgelist
     push!(connections, vert_blocks[edge.src].ϕ => edge.block.src)
     push!(connections, vert_blocks[edge.dst].ϕ => edge.block.dst)
@@ -90,8 +112,9 @@ end
 
 #=
 We want the `connect_system` to get rid of the algebraic states for the edges. Therefore
-we have to provide a list of `outputs` which only contains the outputs of the vertices. By doing
-so the edge outputs will become `istates` of the `IOSystem` and upon connection might be reduced.
+we have to provide a list of `outputs` which only contains the outputs of the vertices. By
+doing so the edge outputs will become internal `istates` of the `IOSystem` and upon
+connection may be reduced.
 =#
 outputs = [block.ϕ for block in vert_blocks]
 
@@ -105,7 +128,8 @@ nothing #hide
 gen = generate_io_function(networkblock,
                            f_states=[v.ϕ for v in vert_blocks],
                            f_params=vcat([v.ω for v in vert_blocks],
-                                         [e.K for e in edge_blocks]))
+                                         [e.K for e in edge_blocks]),
+                           warn=false);
 nothing #hide
 
 # ... enclose the `f_ip` to get rid of the empty `inputs` field...
