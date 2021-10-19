c923600dabf97a9bde9ffecd2f415aea2ab1998e
Hans Würfel <git@wuerfel.io>
2021-10-19 21:42:04 +0200

    test ci handling of env

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,11 @@
+@show ENV
 using BlockSystems
 using Test
 using ModelingToolkit
 
 const MTK = ModelingToolkit
 
-include("BlockSystems_test.jl")
-include("transformations_test.jl")
-include("function_generation_test.jl")
-include("utils_test.jl")
+# include("BlockSystems_test.jl")
+# include("transformations_test.jl")
+# include("function_generation_test.jl")
+# include("utils_test.jl")
